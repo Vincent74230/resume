85b6191958cae43a8ee32cebbd13bf36e213a686
Vincent74230 <vincent.nowak@hotmail.fr>
2020-08-28 06:56:19 +0200

    ADDED: french page clikable, pushed online

--- vincentResume/views.py
+++ vincentResume/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def index(request):
     template = loader.get_template('vincentResume/index.html')
     return HttpResponse(template.render({}, request))
+
+def cv(request):
+    template = loader.get_template('vincentResume/cv.html')
+    return HttpResponse(template.render({}, request))
